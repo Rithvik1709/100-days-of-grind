a36e32fd74f0a9a542445e3fd59341f0b58eb4e8
rithvik <rithvikbng@gmail.com>
2025-01-23 11:35:00 +0530

    patterns fo pyramid structure

---
+++ DSA/Day-00 Patterns /patters.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+using namespace std;
+
+void pyramid_pattern(int N)
+{
+    // outer loop for all lines
+    for (int i = 0; i < N; i++)
+    {
+        // for printing spaces
+        for (int j =0; j<N-i-1; j++)
+        {
+            cout <<" ";
+        }
+       
+        // For printing the stars in each row
+        for(int j=0;j< 2*i+1;j++){
+            
+            cout<<"*";
+        }
+        
+        // For printing the spaces after the stars in each row
+         for (int j =0; j<N-i-1; j++)
+        {
+            cout <<" ";
+        }
+        cout << endl;
+    }
+}
+
+void inverted_pyramid(int N)
+{
+    // This is the outer loop which will loop for the rows.
+    for (int i = 0; i < N; i++)
+    {
+        // For printing the spaces before stars in each row
+        for (int j =0; j<i; j++)
+        {
+            cout <<" ";
+        }
+       
+        // For printing the stars in each row
+        for(int j=0;j< 2*N -(2*i +1);j++){
+            
+            cout<<"*";
+        }
+        
+        // For printing the spaces after the stars in each row
+         for (int j =0; j<i; j++)
+        {
+            cout <<" ";
+        }
+        
+        cout << endl;
+    }
+}
+
+int main()
+{   
+    int N = 5;
+    pyramid_pattern(N);
+    inverted_pyramid(N);
+
+    return 0;
+}
