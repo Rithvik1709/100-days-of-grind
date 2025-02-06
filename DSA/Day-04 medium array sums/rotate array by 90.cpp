f572093f08595ac21eb1568d769961a980e3e43e
rithvik <rithvikbng@gmail.com>
2025-02-06 19:03:43 +0530

    rotate array

---
+++ DSA/Day-04 medium array sums/rotate array by 90.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void rotate(vector<vector<int>>& matrix) {
+    int n = matrix.size();
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < i; j++) {
+            int temp = matrix[i][j];
+            matrix[i][j] = matrix[j][i];
+            matrix[j][i] = temp;
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n / 2; j++) {
+            int temp = matrix[i][j];
+            matrix[i][j] = matrix[i][n - j - 1];
+            matrix[i][n - j - 1] = temp;
+        }
+    }
+}
+
+int main() {
+    vector<vector<int>> matrix = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+
+    rotate(matrix);
+
+    cout << "Rotated Image:\n";
+    for (int i = 0; i < matrix.size(); i++) {
+        for (int j = 0; j < matrix[i].size(); j++) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
